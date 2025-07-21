af40af23d61c79bb7923a0f618b816abc8564d7b
shreyansh0009 <saurabhsharraf@gmail.com>
2025-07-21 22:09:00 +0530

    basic routes

---
+++ reactRouter/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { NavLink } from 'react-router-dom'
+
+
+function Navbar() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      gap: '1rem',
+      padding: '1rem',
+      background: '#eee',
+      marginBottom: '1rem'
+    }}>
+
+        <NavLink to = "/"> Home </NavLink>
+
+        <NavLink to = "/about"> About </NavLink>
+
+        <NavLink to = "/contact"> Contact Us </NavLink>
+
+        <NavLink to = "/login"> Login </NavLink>
+
+
+    </div>
+  )
+}
+
+export default Navbar
---
+++ reactRouter/src/pages/Notfound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Notfound() {
+  return (
+    <div>Item not found! Error 404!</div>
+  )
+}
+
+export default Notfound
